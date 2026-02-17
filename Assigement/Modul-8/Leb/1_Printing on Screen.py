10f622b40376a9f986c0effd4c1f866851ebe7ef
goswami22 <ketangoswami1705@gmail.com>
2026-02-17 15:05:43 +0530

    17/02/2026

---
+++ Assigement/Modul-8/Leb/1_Printing on Screen.py
@@ -0,0 +1,6 @@
+#Write a Python program to print a formatted string using print() and f-string.
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+course = input("Enter your course: ")
+
+print(f"My name is {name}, I am {age} years old and I am studying {course}.")
---
+++ Assigement/Modul-8/Leb/Practical_Examples/2_Reading_Data_from_Keyboard.py
@@ -0,0 +1,10 @@
+# Write a Python program to read a string, an integer, and a float from the keyboard and display them.
+
+s=input("Enter Your String :")
+i=int(input("Enter Your Integer : "))
+f=float(input("Enter Your Float : "))
+
+
+print("String: ",s)
+print("Integer : ",i)
+print("Float : ",f)
